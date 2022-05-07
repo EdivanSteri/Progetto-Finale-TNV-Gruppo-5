171690ea88f859994d2d3406772e9ef170b967ef
Edivan Steri <edivansteri01@gmail.com>
2022-05-07 19:09:18 +0200

    springboot

---
+++ Springboot/userManagement/src/main/java/com/thenetvalue/userManagement/service/UserService.java
@@ -0,0 +1,67 @@
+package com.thenetvalue.userManagement.service;
+
+import com.thenetvalue.userManagement.dao.UserRepositoryDAO;
+import com.thenetvalue.userManagement.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    UserRepositoryDAO userRepositoryDAO;
+
+   @Autowired
+    public UserService(@Qualifier("dbUserDAO") UserRepositoryDAO userRepositoryDAO) {
+        this.userRepositoryDAO = userRepositoryDAO;
+    }
+
+    public  String addUser(User user){
+       User result = userRepositoryDAO.save(user);
+       if(result != null){
+           return "Utente aggiunto correttamente";
+        }else{
+           return "Utente non aggiunto, errore";
+        }
+    }
+
+    public User getUserById(int id){
+        Optional result = userRepositoryDAO.findById(id);
+       return (User) result.get();
+    }
+
+
+    public Iterable<User> getAllUsers() {
+       return userRepositoryDAO.findAll();
+    }
+
+
+    public String updateUserById(int id, User user) {
+       if(user.getId() == 0){
+           user.setId(id);
+       }
+       User result = userRepositoryDAO.save(user);
+        if(result != null){
+            return "Utente aggiornato correttamente";
+        }else{
+            return "Utente non aggiornato, errore";
+        }
+    }
+
+    public String deleteUserById(int id) {
+        User result = userRepositoryDAO.findById(id).orElse(null);
+        if(result == null){
+            return "Utente non elimato, errore";
+
+        }else{
+            userRepositoryDAO.deleteById(id);
+            return "Utente elimanto correttamente";
+        }
+    }
+
+    public User getUserByUsernameAndPassword(String username, String password) {
+       return userRepositoryDAO.findUserByUsernameAndPassword(username, password);
+    }
+}
